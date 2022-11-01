622a398e8c8f663324498c28c28900c4c276be12
Korean Successors <54975853+koreansuccessors@users.noreply.github.com>
2022-11-01 15:03:26 +0800

    Update mint-initiate.sh

--- mint-initiate.sh
+++ mint-initiate.sh
@@ -5,13 +5,14 @@ sudo apt install preload tlp -y
 sudo apt purge hexchat gnote drawing simple-scan transmission-gtk hypnotix mintwelcome ibus -y
 sudo apt install fcitx fcitx-rime
 sudo apt-get upgrade -y
+sudo tee /etc/systemd/logind.conf <<<'KillUserProcesses=yes'
 sudo tee /etc/modprobe.d/snd-hda-intel.conf <<<'options snd-hda-intel power_save=0'
 sudo tee /etc/modprobe.d/modprobe.conf <<<'options snd-hda-intel model=,generic'
 sudo sh -c 'echo N > /sys/module/snd_hda_intel/parameters/power_save'
 sudo sh -c 'echo N > /sys/module/snd_hda_intel/parameters/power_save_controller'
-sudo apt install nscd -y 
-sudo systemctl enable nscd 
-sudo systemctl start nscd 
+sudo apt install nscd
+sudo systemctl enable nscd
+sudo systemctl start nscd
 sudo touch /etc/apt/trusted.gpg 
 sudo apt-get install ubuntu-restricted-addons -y 
 sudo apt-get install ubuntu-restricted-extras -y 

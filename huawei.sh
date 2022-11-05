1d410faf23935d5bafba183797532827ad6afa8f
Korean Successors <54975853+koreansuccessors@users.noreply.github.com>
2022-11-05 22:12:36 +0800

    Add files via upload

---
+++ huawei.sh
@@ -0,0 +1,33 @@
+adb shell pm disable-user com.huawei.android.hwouc
+adb shell pm uninstall --user 0 com.huawei.phoneservice
+adb shell pm uninstall --user 0 com.huawei.contactscamcard
+adb shell pm uninstall --user 0 com.android.wallpaper.livepicker
+adb shell pm uninstall --user 0 com.android.vending
+adb shell pm uninstall --user 0 com.google.android.gms
+adb shell pm uninstall --user 0 com.android.stk
+adb shell pm uninstall --user 0 com.sina.weibo
+adb shell pm uninstall --user 0 com.huawei.hidisk
+adb shell pm uninstall --user 0 com.huawei.gameassistant
+adb shell pm uninstall --user 0 com.huawei.android.findmyphone
+adb shell pm uninstall --user 0 com.google.ar.core
+adb shell pm uninstall --user 0 com.huawei.tips
+adb shell pm uninstall --user 0 com.huawei.himovie.partner1
+adb shell pm uninstall --user 0 com.huawei.himovie.partner2
+adb shell pm uninstall --user 0 com.huawei.magazine
+adb shell pm uninstall --user 0 com.huawei.hicar
+adb shell pm uninstall --user 0 com.huawei.hicloud
+adb shell pm uninstall --user 0 com.tencent.qqlivehuawei
+adb shell pm uninstall --user 0 com.huawei.android.karaoke
+adb shell pm uninstall --user 0 com.huawei.game.kitserver
+adb shell pm uninstall --user 0 com.huawei.skytone
+adb shell pm uninstall --user 0 com.amap.android.ams
+adb shell pm uninstall --user 0 ctrip.android.view
+adb shell pm uninstall --user 0 com.android.hwmirror
+adb shell pm uninstall --user 0 com.szzc.ucar.pilot
+adb shell pm uninstall --user 0 com.android.calculator2
+adb shell pm uninstall --user 0 com.huawei.hwvplayer.youku
+adb shell pm uninstall --user 0 com.sohu.sohuvideo.emplayer
+adb shell pm uninstall --user 0 com.huawei.gamebox
+adb shell pm uninstall --user 0 com.huawei.hwireader
+adb shell pm uninstall --user 0 com.huawei.hiskytone
+

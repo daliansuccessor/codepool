721cdcc6486f4cf7e97b57fe94fffddcb325e849
Distributorship <54975853+distributorship@users.noreply.github.com>
2023-05-10 08:21:36 +0800

    Update oraclecloud.sh

--- oraclecloud.sh
+++ oraclecloud.sh
@@ -10,6 +10,15 @@ echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_fastopen=3" >> /etc/sysctl.conf
 sysctl -p
+lsmod | grep bbr
+sudo modprobe tcp_bbr
+echo "tcp_bbr" | sudo tee --append /etc/modules-load.d/modules.conf
+echo "net.core.default_qdisc=fq" | sudo tee --append /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" | sudo tee --append /etc/sysctl.conf
+sudo sysctl -p
+sysctl net.ipv4.tcp_available_congestion_control
+sysctl net.ipv4.tcp_congestion_control
+lsmod | grep bbr
 echo '* - nofile 65535 ' >>/etc/security/limits.conf
 echo "* soft nproc 65535" >> /etc/security/limits.conf
 echo "* hard nproc 65535" >> /etc/security/limits.conf
